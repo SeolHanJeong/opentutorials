82780bd042c60a61981644f5b0d4ed1e99ec537f
SeolHanJeong <dkfzpdls@naver.com>
2017-08-02 14:34:14 +0900

    SELECT UPDATA  INSERT DELETE practice

--- nodejs/server_side_javascript/database_mysqldb/database_mysqldb.js
+++ nodejs/server_side_javascript/database_mysqldb/database_mysqldb.js
@@ -9,13 +9,50 @@ var conn = mysql.createConnection({
 
 conn.connect();
 
-var sql = 'SELECT * FROM topic';
-conn.query(sql, function(error, results, fields) {
-  if (error) {
-    console.log(err);
-  } else {
-    console.log('results', results);
-    console.log('fields', fields);
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = ['7'];
+
+conn.query(sql,params,function(error,results,fields){
+  if(error){
+    console.log(error);
+  }
+  else{
+    console.log(results);
   }
 });
+
+// var sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+// var params = ['javascript','mincha','7'];
+//
+// conn.query(sql,params,function(error,results,fields){
+//   if(error){
+//     console.log(error);
+//   }
+//   else{
+//     console.log(results);
+//   }
+// });
+
+// var sql = 'INSERT INTO topic (title,description,author) VALUES(?,?,?)';
+// var params = ['Supervisor','Watcher','graphitie'];
+//
+// conn.query(sql,params,function(error,results,fields){
+//   if(error){
+//     console.log(error);
+//   }
+//   else{
+//     console.log(results.insertId);
+//   }
+// });
+
+// var sql = 'SELECT * FROM topic';
+// conn.query(sql, function(error, results, fields) {
+//   if (error) {
+//     console.log(err);
+//   } else {
+//     for(var i = 0; i<results.length; i++){
+//       console.log(results[i].description);
+//     }
+//   }
+// });
 conn.end();
